a060dc4cef0ba61da1b4330d2e8a428b99701560
qwertpi <qwertpi@users.noreply.github.com>
2018-10-14 11:07:40 +0100

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,10 @@
+i=1
+def DivsiableCheck(num):
+    for i in range(1,21):
+        if num%i!=0:
+            return False
+            break
+    return True
+while DivsiableCheck(i)!=True:
+    i+=1
+print(i)
